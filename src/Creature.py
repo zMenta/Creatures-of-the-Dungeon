78f66b661bd272c425bb621a25992747bee6fcfb
brentano.l@gmail.com <70714721+zMenta@users.noreply.github.com>
2021-12-04 08:50:03 -0300

    Creature Class

--- src/Creature.py
+++ src/Creature.py
@@ -2,24 +2,31 @@
 class Creature:
     """
     Class of a Creature.
-
     It initializes with strength and vitality attributes.
     """
     def __init__(self, strength, vitality) -> None:
         self.strength = strength
         self.vitality = vitality
 
-        self.health = vitality*3
+        self.health = round(vitality*3)
         self.attack = round(strength*1.5)
+
+        self.equipment = None
 
 
     def info(self) -> dict:
+        """
+        Returns the info and stats of the creature.
+        Returns:
+            dict
+        """
         info = {
             "strength": self.strength,
             "vitality": self.vitality,
             "health": self.health,
-            "attack": self.attack
+            "attack": self.attack,
+            "equipment": self.equipment
         }
-
+        
         return info
 
