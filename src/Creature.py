3eabd97585fa2e220e0bba563562653c3fd7c3bd
brentano.l@gmail.com <70714721+zMenta@users.noreply.github.com>
2021-12-04 09:50:24 -0300

    Creature Methods

--- src/Creature.py
+++ src/Creature.py
@@ -2,16 +2,15 @@
 class Creature:
     """
     Class of a Creature.
-    It initializes with strength and vitality attributes.
+    It initializes with strength,vitality and name attributes.
     """
-    def __init__(self, strength, vitality) -> None:
+    def __init__(self, strength, vitality, name) -> None:
+        self.name = name
         self.strength = strength
         self.vitality = vitality
 
         self.health = round(vitality*3)
-        self.attack = round(strength*1.5)
-
-        self.equipment = None
+        self.attack_damage = round(strength*1.5)
 
 
     def info(self) -> dict:
@@ -21,12 +20,23 @@ class Creature:
             dict
         """
         info = {
+            "name": self.name,
             "strength": self.strength,
             "vitality": self.vitality,
             "health": self.health,
-            "attack": self.attack,
-            "equipment": self.equipment
+            "attack_damage": self.attack_damage
         }
         
         return info
+
+
+    def attack(self, target) -> None:
+        """
+        Attacks the Creature in the paremeter.
+        """
+        target.health -= self.attack_damage
+
+        print(f"{self.name} Attacked {target.name} and dealt {self.attack_damage} damage!")
+        
+        
 
--- src/main.py
+++ src/main.py
@@ -2,9 +2,14 @@
 from Creature import Creature
 
 def main():
-    guy = Creature(8,15)
+    guy = Creature(8,15, "player")
+    enemy = Creature(4,5,"enemy")
 
     print(guy.info())
+    print(enemy.info())
+
+    guy.attack(enemy)
+    print(enemy.info())
 
 if __name__ == "__main__":
     main()
